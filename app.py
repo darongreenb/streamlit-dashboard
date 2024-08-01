6e9b0581aa26227fc1dc8561a6e74edc85e439b2
acv1229 <137930447+acv1229@users.noreply.github.com>
2024-08-01 12:32:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if page == "GreenAleph Active Principal":
 
     SELECT 
         l.LeagueName,
-        ROUND(SUM(DollarsAtStake), 0) AS TotalDollarsAtStake
+        ROUND(SUM(DollarsAtStake)) AS TotalDollarsAtStake
     FROM 
         DistinctBets db
     JOIN 
@@ -59,7 +59,7 @@ if page == "GreenAleph Active Principal":
 
     SELECT 
         'Total' AS LeagueName,
-        ROUND(SUM(DollarsAtStake), 0) AS TotalDollarsAtStake
+        ROUND(SUM(DollarsAtStake)) AS TotalDollarsAtStake
     FROM 
         DistinctBets;
     """
