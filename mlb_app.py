19574dea7ebfea4d5d28dedbc3783511c2f55b54
acv1229 <137930447+acv1229@users.noreply.github.com>
2024-08-05 12:38:32 -0400

    Update mlb_app.py

--- mlb_app.py
+++ mlb_app.py
@@ -236,7 +236,7 @@ elif page == "MLB Principal Charts":
         # Add border around the plot
         for spine in ax.spines.values():
             spine.set_edgecolor('black')
-            spine.set linewidth(1.2)
+            spine.set_linewidth(1.2)
 
         # Adjust layout
         plt.tight_layout()
