196c3395b07815bb0df474c1ca24413221686012
acv1229 <137930447+acv1229@users.noreply.github.com>
2024-08-07 13:44:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -327,7 +327,7 @@ elif page == "MLB Charts":
                         bars2 = ax.bar(combined_df['ParticipantName'], combined_df['TotalDollarsAtStake'], color=color_dollars_at_stake, width=0.4, edgecolor='black', label='Total Dollars At Stake')
                         
                         # Add labels and title
-                        ax.set_title(f'Total Active Principal and Potential Payout by ParticipantName for {event_type_option} - {event_label_option} (GA1, Straight Bets Only)', fontsize=18, fontweight='bold')
+                       
                         ax.set_ylabel('Total Amount ($)', fontsize=16, fontweight='bold')  # Enlarge y-axis label
                         
                         # Annotate each bar with the value (no dollar sign)
