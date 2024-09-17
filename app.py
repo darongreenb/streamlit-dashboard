aa18d179f996b2288331851439dd319cd1d0c418
acv1229 <137930447+acv1229@users.noreply.github.com>
2024-09-17 14:30:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ elif page == "NFL Charts":
                                        color=color_potential_payout, width=0.4, edgecolor='black')
                 
                         # Add labels and title
-                        ax.set_ylabel('Total Amount ($)', fontsize=16, fontweight='bold')
+                        ax.set_ylabel('USD ($)', fontsize=16, fontweight='bold')
                         ax.set_title(f'Total Active Principal and Potential Payout by ParticipantName for {event_type_option} - {event_label_option} (GA1, Straight Bets Only)', fontsize=18, fontweight='bold')
                 
                         # Annotate each bar with the TotalDollarsAtStake value below the bar
