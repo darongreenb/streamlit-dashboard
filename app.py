d92d768a1a4b6df1c4df0c8894e79eb4ab050fb3
acv1229 <137930447+acv1229@users.noreply.github.com>
2024-11-13 18:27:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1112,7 +1112,7 @@ elif page == "NFL Charts":
                             spine.set_linewidth(1.2)
                 
                         # Extend y-axis range
-                        ax.set_ylim(min(combined_df['TotalDollarsAtStake']) - 5000, max(combined_df['TotalPotentialPayout']) + 5000)
+                        ax.set_ylim(min(combined_df['TotalDollarsAtStake']) - 5000, max(combined_df['TotalPotentialPayout']) + 10000)
                 
                         # Adjust layout
                         plt.tight_layout()
